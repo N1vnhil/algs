5dadbe47348ffe88a261a11cdf74538a6fdc52d1
Gnehihz Niaq <n1vnhi@outlook.com>
2024-05-09 22:00:38 +0800

    dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -7,43 +7,45 @@ using namespace std;
 
 // graph: s_v[(e_v, edge)]
 vector<int> dijkstra(vector<vector<pair<int, int>>> graph, int start, int end){
-    vector<int> edgeTo(graph.size(), -1), distTo(graph.size(), INT_MAX), isMarked(graph.size(), 0);
-    isMarked[start] = 1;
+    vector<int> distTo(graph.size(),INT_MAX), edgeTo(graph.size(),-1), isMarked(graph.size(),0);
     distTo[start] = 0;
-    vector<int> arr;
-    pair<int, int> stV;
+    bool allMarked=false;
     int node = start;
-
-    while(node!=-1){
+    while(node!=end){
         isMarked[node] = 1;
-        arr.push_back(node);
-        if(node==end){
-            cout << "SP: " << distTo[node] << endl;
-            return arr;
-        }
-
-        pair<int,int> stV(-1, INT_MAX);
-        bool allMarked = true;
-        for(pair<int,int> i: graph[node]){
-            if(!isMarked[i.first]){
-                allMarked = false;
-                if(i.second<stV.second) stV = i;
-                if(distTo[node]+i.second < distTo[i.first]){
-                    distTo[i.first] = distTo[node]+i.second;
-                    edgeTo[i.first] = node;
-                }
+        for(pair<int,int> i:graph[node]){
+            if(distTo[node]+i.second<distTo[i.first]){
+                distTo[i.first] = distTo[node]+i.second;
+                edgeTo[i.first] = node;
             }
         }
-        if(allMarked){
-            arr.pop_back();
-            if(!arr.empty()) node = arr.back();
-            else node = -1;
-        }else node = stV.first;
+
+        int next = -1, dist = INT_MAX;
+        for(int i=0; i<graph.size(); i++){
+            if(!isMarked[i] && distTo[i]<dist){
+                next = i;
+                dist =  distTo[i];
+            }
+        }
+
+        if(next==-1){
+            cout << start << " and " << end << " not connected." << endl;
+            return {};
+        }
+
+        node = next;
     }
 
-    arr.push_back(-1);
+    vector<int> arr;
+    cout << "dist: " << distTo[end] << endl;
+    while(end!=-1){
+        arr.push_back(end);
+        end = edgeTo[end];
+    }
+    reverse(arr.begin(),arr.end());
     return arr;
 }
+
 
 
 int main(){
@@ -60,7 +62,7 @@ int main(){
     cout << endl;
     for(int i:dijkstra(g,1,3)) cout << i << " ";
     cout << endl;
-    for(int i:dijkstra(g,5,3)) cout << i << " ";
+    for(int i:dijkstra(g,0,1)) cout << i << " ";
     cout << endl;
     for(int i:dijkstra(g,3,0)) cout << i << " ";
 }
