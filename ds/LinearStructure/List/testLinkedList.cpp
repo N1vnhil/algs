84514f61a746f9bbebdd921cb16c5bce4ab4816a
Gnehihz Niaq <n1vnhi@outlook.com>
2024-10-10 18:05:19 +0800

    update

--- ds/LinearStructure/List/testLinkedList.cpp
+++ ds/LinearStructure/List/testLinkedList.cpp
@@ -1,27 +1,23 @@
 #define CRTDBG_MAP_ALLOC
 #include<iostream>
 #include<crtdbg.h>
+#include<string>
 #include"LinkedList.cpp"
 
 using namespace std;
 
 int main() {
-    LinkedList<int> arr;
-    cout << arr.length() << endl;
-    for(int i=0; i<5; i++) arr.insert(0, i);
-    arr.traverse();
-    arr.visit(0);
-    cout << arr.search(3) << endl;
-
-    arr.remove(3);
-    arr.remove(3);
-    cout << arr.length() << endl;
-    cout << arr.search(1) << endl;
+    LinkedList<char> arr;
+    string str = "apple";
+    for(char ch: str) arr.insert(arr.length(), ch);
     arr.traverse();
 
+    arr.insert(arr.length(), 'x');
+    arr.remove(arr.length()-2);
+    cout << arr.visit(arr.length()-1) << endl;
     arr.clear();
-    arr.traverse();
     cout << arr.length() << endl;
+    arr.traverse();
 
     _CrtDumpMemoryLeaks();
     system("pause");
